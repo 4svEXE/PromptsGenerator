9c6a4c50de602ca38bae979d60b5deacabadc88a
4svEXE <4sv.exe@gmail.com>
2024-08-13 17:41:41 +0200

    upd

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,17 +1,13 @@
 import { ReactNode } from "react";
 
 import WithRouter from "./withRouter";
-import WithTheme from "./withTheme";
+
 import WithLayout from "./withLayout";
 
 export function ConnectProwiders({ children }: { children: ReactNode }) {
   return (
     <WithRouter>
-      <WithTheme>
-        {({ toggleTheme }) => (
-          <WithLayout children={children} toggleTheme={toggleTheme} />
-        )}
-      </WithTheme>
+      <WithLayout children={children} />
     </WithRouter>
   );
 }
